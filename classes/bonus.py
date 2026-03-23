466c0a20da94de4254ee4e2d94b3a1e01fa5fb33
KylianRigault <127503349+KylianRigault@users.noreply.github.com>
2026-03-23 15:32:55 +0100

    Refactor Bonus class with movement and hitbox
    Refactor Bonus class to include movement functionality and update hitbox handling.

--- classes/bonus.py
+++ classes/bonus.py
@@ -1,13 +1,16 @@
+import pygame
+import random
+
+SPEED = 2
+
 class Bonus:
     def __init__(self, x, y, image, bonus_type):
-        self.x = x
-        self.y = y
         self.image = image
         self.bonus_type = bonus_type
-        self.rect = self.image.get_rect(topleft=(x, y))
-
+        self.hitbox = self.image.get_rect(topleft=(x,y))
+    
     def apply(self, player):
         player.current_bonus = self.bonus_type
 
-    def check_collision(self, other_rect):
-        return self.rect.colliderect(other_rect)
+    def move(self):
+        self.hitbox.x -= SPEED
